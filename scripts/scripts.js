f0adf7f0ae4fe977550fac9c00b8f93ed96b18e6
Viktoriya Lavrukhina <tomenko_viktoriya.ru>
2022-01-18 00:30:32 +0300

    feat: добавить адаптив для mobile, readme.md

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,8 +6,8 @@ function openPopup() {
   popup.classList.add('popup_opened');
   let nameInput = document.querySelector('.profile__name').textContent;
   document.querySelector('.input__text_type_name').value = nameInput;
-  let jobInput = document.querySelector('.profile__about').textContent
-  document.querySelector('.input__text_type_about').value = jobInput;
+  let jobInput = document.querySelector('.profile__job').textContent
+  document.querySelector('.input__text_type_job').value = jobInput;
 }
 
 function closePopup() {
@@ -18,12 +18,12 @@ OpenPopupButton.addEventListener('click', openPopup)
 ClosePopupButton.addEventListener('click', closePopup)
 
 function input() {
-let input_name = document.querySelector('.input__text_type_name').value;
-document.querySelector('.profile__name').textContent= input_name;
-let input_about = document.querySelector('.input__text_type_about').value;
-document.querySelector('.profile__about').textContent = input_about;
-popup.classList.remove('popup_opened');
+  let input_name = document.querySelector('.input__text_type_name').value;
+  document.querySelector('.profile__name').textContent= input_name;
+  let input_job = document.querySelector('.input__text_type_job').value;
+  document.querySelector('.profile__job').textContent = input_job;
+  popup.classList.remove('popup_opened');
 }
 
-const SubmitPopupButton = document.querySelector('.popup__submit-btn');
-SubmitPopupButton.addEventListener('click', input);
+const inputPopupButton = document.querySelector('.input');
+inputPopupButton.addEventListener('submit', input);
