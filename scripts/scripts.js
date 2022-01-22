e816a128cd12c99a45d82917f777c35728830005
Viktoriya Lavrukhina <tomenko_viktoriya.ru>
2022-01-22 22:08:28 +0300

    Исправить ошибки после ревью

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,30 +1,29 @@
-const OpenPopupButton = document.querySelector('.profile__button-edit')
-const popup = document.querySelector('.popup')
-const ClosePopupButton = document.querySelector('.popup__close-btn')
+const openPopupButton = document.querySelector('.profile__button-edit');
+const popup = document.querySelector('.popup');
+const closePopupButton = document.querySelector('.popup__close-btn');
+const nameInput = document.querySelector('.profile__name');
+const jobInput = document.querySelector('.profile__job');
+const inputName = document.querySelector('.input__text_type_name');
+const inputJob = document.querySelector('.input__text_type_job');
+const inputPopupButton = document.querySelector('.input');
 
 function openPopup() {
   popup.classList.add('popup_opened');
-  let nameInput = document.querySelector('.profile__name').textContent;
-  document.querySelector('.input__text_type_name').value = nameInput;
-  let jobInput = document.querySelector('.profile__job').textContent
-  document.querySelector('.input__text_type_job').value = jobInput;
+  inputName.value = nameInput.textContent;
+  inputJob.value = jobInput.textContent;
 }
 
 function closePopup() {
-  popup.classList.remove('popup_opened')
-}
-
-OpenPopupButton.addEventListener('click', openPopup)
-ClosePopupButton.addEventListener('click', closePopup)
-
-function input(evt) {
-  evt.preventDefault();
-  let input_name = document.querySelector('.input__text_type_name').value;
-  document.querySelector('.profile__name').textContent= input_name;
-  let input_job = document.querySelector('.input__text_type_job').value;
-  document.querySelector('.profile__job').textContent = input_job;
   popup.classList.remove('popup_opened');
 }
 
-const inputPopupButton = document.querySelector('.input');
-inputPopupButton.addEventListener('submit', input);
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  nameInput.textContent = inputName.value;
+  jobInput.textContent = inputJob.value;
+  closePopup();
+}
+
+inputPopupButton.addEventListener('submit', formSubmitHandler);
+openPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
